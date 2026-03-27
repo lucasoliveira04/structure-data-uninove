04edf219c81994bd55f146b4fe81940a7d4b1fd5
lucasoliveira04 <lucasolisocialmedia@gmail.com>
2026-03-27 00:32:33 -0300

    Add `delete_value` and `search_value`

--- linkedlists/main.c
+++ linkedlists/main.c
@@ -42,17 +42,6 @@ void list_prepend(LinkedList *list, int value) {
     list->size++;
 }
 
-// Print O(n)
-void list_print(const LinkedList *list) {
-    Node *curr = list->head;
-
-    while (curr) {
-        printf("[%d] -> ", curr->value);
-        curr = curr->next;
-    }
-    printf("NULL\n");
-}
-
 // Append O(1)
 void list_append(LinkedList *list, int value) {
     Node *node = malloc(sizeof(Node));
@@ -71,6 +60,58 @@ void list_append(LinkedList *list, int value) {
     list->size++;
 }
 
+int search_value(const LinkedList *list, int value) {
+    Node *curr = list->head;
+
+    if (!curr->value) {
+        return -1;
+    }
+
+    if (curr->value == value) {
+        return curr->value;
+    }
+
+    while (curr->next) {
+        if (curr->value == value) {
+            return curr->value;
+        }
+
+        curr = curr->next;
+    }
+}
+
+void delete_value(LinkedList *list, int value) {
+    Node *curr = list->head;
+    Node *prev = NULL;
+
+    if (!curr) {
+        return;
+    }
+
+    while (curr) {
+        if (curr->value == value) {
+            if (!prev) {
+                list->head = curr->next;
+                if (!list->head) {
+                    list->tail = NULL;
+                }
+            } else {
+                prev->next = curr->next;
+                if (!curr->next) {
+                    list->tail = prev;
+                }
+            }
+
+            free(curr);
+            list->size--;
+            return;
+        }
+
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
 // Destructor
 void list_free(LinkedList *list) {
     Node *curr = list->head;
@@ -82,6 +123,16 @@ void list_free(LinkedList *list) {
     free(list);
 }
 
+// Print O(n)
+void list_print(const LinkedList *list) {
+    Node *curr = list->head;
+
+    while (curr) {
+        printf("[%d] -> ", curr->value);
+        curr = curr->next;
+    }
+    printf("NULL\n");
+}
 
 int main() {
     LinkedList *list = list_create();
@@ -94,6 +145,18 @@ int main() {
     list_print(list);
 
     list_append(list, 20);
+
+    list_print(list);
+
+    // int searchValue = 230;
+
+    // if (search_value(list, searchValue)) {
+    //     printf("%d", searchValue);
+    // } else {
+    //     printf("Valor não encontrado");
+    // }
+
+    delete_value(list, 1);
 
     list_print(list);
 }
